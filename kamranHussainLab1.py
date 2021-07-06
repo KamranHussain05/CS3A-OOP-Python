887465af748f7358e784ad74a0bee07e3c642e84
KamranHussain05 <38573620+KamranHussain05@users.noreply.github.com>
2021-07-05 21:46:50 -0700

    Added Lab 1

---
+++ kamranHussainLab1.py
@@ -0,0 +1,16 @@
+###########################################################
+# Course: CS 3A Object-Oriented Programming Methodologies in Python
+# Lab 1 Compiler, Tabs, Indentation, User Output
+# Name: Kamran Hussain
+# Description:  A series of print statements to test the functionality of the python environment, virtual machine, and runtime.
+# Date: 7/5/2021
+# ###########################################################
+
+print('Hello, World!')
+print('Family Name: Hussain')
+print('Foothill Student ID: 20495931')
+print('Syllabus Detail 1: ')
+print('Syllabus Detail 2: ')
+print('Coding Standards Details 1: ')
+print('Coding Standards Details 2: ')
+print('Coding Standards Details 3: ')
