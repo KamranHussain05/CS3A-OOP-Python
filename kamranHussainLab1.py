c22c086cf371f7c1fce1d37303021108e485060d
KamranHussain05 <38573620+KamranHussain05@users.noreply.github.com>
2021-07-05 23:10:27 -0700

    Updated Lab 1

--- kamranHussainLab1.py
+++ kamranHussainLab1.py
@@ -1,16 +1,40 @@
 ###########################################################
-# Course: CS 3A Object-Oriented Programming Methodologies in Python
-# Lab 1 Compiler, Tabs, Indentation, User Output
 # Name: Kamran Hussain
-# Description:  A series of print statements to test the functionality of the python environment, virtual machine, and runtime.
 # Date: 7/5/2021
+# Course: CS 3A Object-Oriented Programming Methodologies in Python
+# Lab 1: Compiler, Tabs, Indentation, User Output
+# Description: A series of print statements to test the functionality of the
+#       python environment, virtual machine, and runtime.
 # ###########################################################
 
+# Program Source Code:
 print('Hello, World!')
-print('Family Name: Hussain')
-print('Foothill Student ID: 20495931')
-print('Syllabus Detail 1: ')
-print('Syllabus Detail 2: ')
-print('Coding Standards Details 1: ')
-print('Coding Standards Details 2: ')
-print('Coding Standards Details 3: ')
+print('My Family Name: Hussain')
+print('My Foothill Student ID: 20495931\n')
+print('Syllabus Detail 1: The Course CRN: 10433')
+print('Syllabus Detail 2: The textbook for the course is, Starting Out with \n'
+      'Python, 5th edition, by Tony Gaddis.\n')
+print('Coding Standards Details 1: Source code lines will be no more than 79 \n'
+      'characters long')
+print('Coding Standards Details 2: Use “CamelCase” style for all class names')
+print('Coding Standards Details 3: Each source code file will contain an \n'
+      'introductory block of comments that identifies the assignment and \n'
+      'gives a brief overview of the program')
+
+# Program Console Output:
+'''
+Hello, World!
+My Family Name: Hussain
+My Foothill Student ID: 20495931
+
+Syllabus Detail 1: The Course CRN: 10433
+Syllabus Detail 2: The textbook for the course is, Starting Out with 
+Python, 5th edition, by Tony Gaddis.
+
+Coding Standards Details 1: Source code lines will be no more than 79 
+characters long
+Coding Standards Details 2: Use “CamelCase” style for all class names
+Coding Standards Details 3: Each source code file will contain an 
+introductory block of comments that identifies the assignment and 
+gives a brief overview of the program
+'''
