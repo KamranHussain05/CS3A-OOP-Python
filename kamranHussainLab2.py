3a4bf3325513911491cb00d6524edc04118a4f8f
KamranHussain05 <38573620+KamranHussain05@users.noreply.github.com>
2021-07-13 16:57:27 -0700

    Update kamranHussainLab2.py

--- kamranHussainLab2.py
+++ kamranHussainLab2.py
@@ -8,39 +8,42 @@
 # ###########################################################
 
 # Source code:
+def main():  # alternative function name is groceryCouponCalculator
+    n = float(input('Please enter the cost of your groceries: $'))
 
-n = float(input('Please enter the cost of your groceries: $'))
+    while n<0:
+        n = float(input('The value you entered is negative, please enter a '
+                        'positive value: '))
 
-while n<0:
-    n = float(input('The value you entered is negative, please enter a '
-                    'positive value: '))
-
-if n < 10.0:
-    print('You win a discount coupon of $0.00 (0% of your purchase)')
-elif 10 <= n < 60:
-    percentage = 0.08
-    discount = n*percentage
-    formatted_value = "{:.2f}".format(discount)
-    print('You win a discount coupon of $' + str(formatted_value) +
-          ' (8% of your purchase)')
-elif 60 <= n < 150:
-    percentage = 0.10
-    discount = n*percentage
-    formatted_value = "{:.2f}".format(discount)
-    print('You win a discount coupon of $' + str(formatted_value) +
-          ' (10% of your purchase)')
-elif 150 <= n < 210:
-    percentage = 0.12
-    discount = n*percentage
-    formatted_value = "{:.2f}".format(discount)
-    print('You win a discount coupon of $' + str(formatted_value) +
-          ' (12% of your purchase)')
-elif n >= 210:
-    percentage = 0.14
-    discount = n*percentage
-    formatted_value = "{:.2f}".format(discount)
-    print('You win a discount coupon of $' + str(formatted_value) +
-          ' (14% of your purchase)')
+    if n < 10.0:
+        print('You win a discount coupon of $0.00 (0% of your purchase)')
+    elif 10 <= n < 60:
+        percentage = 0.08
+        discount = n*percentage
+        formatted_value = "{:.2f}".format(discount)
+        print('You win a discount coupon of $' + str(formatted_value) +
+              ' (8% of your purchase)')
+    elif 60 <= n < 150:
+        percentage = 0.10
+        discount = n*percentage
+        formatted_value = "{:.2f}".format(discount)
+        print('You win a discount coupon of $' + str(formatted_value) +
+              ' (10% of your purchase)')
+    elif 150 <= n < 210:
+        percentage = 0.12
+        discount = n*percentage
+        formatted_value = "{:.2f}".format(discount)
+        print('You win a discount coupon of $' + str(formatted_value) +
+              ' (12% of your purchase)')
+    elif n >= 210:
+        percentage = 0.14
+        discount = n*percentage
+        formatted_value = "{:.2f}".format(discount)
+        print('You win a discount coupon of $' + str(formatted_value) +
+              ' (14% of your purchase)')
+# program entry point
+if __name__=='__main__':
+    main()
 
 # Sample Run:
 '''
