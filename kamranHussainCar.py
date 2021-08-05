4a9a0aa4f7876f8f220572424af6cd0d1e84b85a
KamranHussain05 <38573620+KamranHussain05@users.noreply.github.com>
2021-08-05 11:32:24 -0700

    Added Car Lab

---
+++ kamranHussainCar.py
@@ -0,0 +1,15 @@
+#############################################################
+# Name: Kamran Hussain
+# Date: 8/3/2021
+# Course: CS 3A Object-Oriented Programming Methodologies in Python
+# Lab 4: Classes and Object-Oriented Programming
+# Description:
+# ###########################################################
+
+class Car:
+    make = str(' ')
+    color = str(' ')
+    doors = int(' ')
+
+    def main(self):
+        print('test')
